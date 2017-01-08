6ca4e440a08a7f8650c642478f2327c3f7854d98
masasin <mshtsy+github@gmail.com>
2017-01-09 02:05:52 +0900

    Initial commit.

--- pyrosbag/pyrosbag.py
+++ pyrosbag/pyrosbag.py
@@ -1,1 +1,251 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Programmatically play and control a ROS bag file.
+
+Note that, in order to access the data within the bag file, the rosbag_python
+package is extremely convenient. It is available on PyPI.
+
+"""
+import logging
+import subprocess as sp
+import time
+
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+logger = logging.getLogger("bag_player")
+
+
+class BagError(Exception):
+    """
+    Catch bag player exceptions.
+
+    """
+
+
+class BagNotPlayingError(BagError, AttributeError):
+    """
+    Raised when interaction is attempted with a bag file which is not playing.
+
+    """
+    def __init__(self, action="talk to"):
+        message = "Cannot {} process while bag is not playing.".format(action)
+        super(BagNotPlayingError, self).__init__(message)
+
+
+class Bag(object):
+    """
+    Open and manipulate a bag file programmatically.
+
+    Parameters
+    ----------
+    filenames : str | List[str]
+        The location of the bag files.
+
+    Attributes
+    ----------
+    filenames : List[str]
+        The location of the bag files.
+    process : subprocess.Popen
+        The process containing the playing bag file.
+
+    """
+    def __init__(self, filenames):
+        if isinstance(filenames, str):
+            filenames = [filenames]
+        self.filenames = filenames
+        self.process = None
+
+    def play(self, wait=False, stdin=sp.PIPE, stdout=None, stderr=None,
+             quiet=None, immediate=None, start_paused=None, queue_size=None,
+             publish_clock=None, clock_publish_freq=None, delay=None,
+             publish_rate_multiplier=None, start_time=None, duration=None,
+             loop=None, keep_alive=None):
+        """
+        Play the bag file.
+
+        Parameters
+        ----------
+        wait : Optional[Bool]
+            Wait until completion.
+        stdin : Optional[file]
+            The stdin buffer. Default is subprocess.PIPE.
+        stdout : Optional[file]
+            The stdout buffer.
+        stderr : Optional[file]
+            The stderr buffer.
+        quiet : Optional[Bool]
+            Suppress console output.
+        immediate : Optional[Bool]
+            Play back all messages without waiting.
+        start_paused : Optional[Bool]
+            Start in paused mode.
+        queue_size : Optional[int]
+            Set outgoing queue size. Default is 100.
+        publish_clock : Optional[Bool]
+            Publish the clock time.
+        clock_publish_freq : Optional[float]
+            The frequency, in Hz, at which to publish the clock time. Default is
+            100.
+        delay : Optional[float]
+            The number of seconds to sleep afer every advertise call (e.g., to
+            allow subscribers to connect).
+        publish_rate_multiplier : Optional[float]
+            The factor by which to multiply the publish rate.
+        start_time : Optional[float]
+            The number of seconds into the bag file at which to start.
+        duration : Optional[float]
+            The number of seconds from the start to play.
+        loop : Optional[Bool]
+            Loop playback.
+        keep_alive : Optional[Bool]
+            Keep alive past end of bag (e.g. for publishing latched topics).
+
+        """
+        arguments = ["rosbag", "play"]
+        arguments.extend(self.filenames)
+
+        if quiet:
+            arguments.append("-q")
+        if immediate:
+            arguments.append("-i")
+        if start_paused:
+            arguments.append("--pause")
+        if queue_size is not None:
+            arguments.append("--queue={}".format(queue_size))
+        if publish_clock:
+            arguments.append("--clock")
+        if clock_publish_freq:
+            arguments.append("--hz={}".format(clock_publish_freq))
+        if delay:
+            arguments.append("--delay={}".format(delay))
+        if publish_rate_multiplier:
+            arguments.append("--rate={}".format(publish_rate_multiplier))
+        if start_time:
+            arguments.append("--start={}".format(start_time))
+        if duration:
+            arguments.append("--duration={}".format(duration))
+        if loop:
+            arguments.append("-l")
+        if keep_alive:
+            arguments.append("-k")
+
+        self.process = sp.Popen(arguments,
+                                stdin=stdin, stdout=stdout, stderr=stderr)
+        if wait:
+            self.wait()
+
+    def send(self, string):
+        """
+        Write something to process stdin.
+
+        Parameters
+        ----------
+        string : str
+            The string to write.
+
+        Raises
+        ------
+        BagNotPlayingError
+            If interaction is attempted when the bag file is not playing.
+
+        """
+        try:
+            self.process.stdin.write(string)
+        except AttributeError:
+            raise BagNotPlayingError()
+
+    def pause(self):
+        """
+        Pause the bag file.
+
+        """
+        self.send(" ")
+
+    def resume(self):
+        """
+        Resume the bag file.
+
+        """
+        self.send(" ")
+
+    def step(self):
+        """
+        Step through a paused bag file.
+
+        """
+        self.send("s")
+
+    def stop(self):
+        """
+        Stop a playing bag file.
+
+        Raises
+        ------
+        BagNotPlayingError
+            If the bag file is not playing.
+
+        """
+        try:
+            self.process.terminate()
+            self.process.kill()
+        except AttributeError:
+            raise BagNotPlayingError("stop")
+
+    def wait(self):
+        """
+        Block until process is complete.
+
+        """
+        self.process.wait()
+
+    @property
+    def is_playing(self):
+        """
+        Check whether the bag file is playing.
+
+        Returns
+        -------
+        bool
+            The bag file is playing.
+
+        """
+        return self.process.poll() is None
+
+    def __enter__(self):
+        """
+        Context manager entry point.
+
+        """
+        return self
+
+    # noinspection PyUnusedLocal
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Context manager exit point.
+
+        """
+        time.sleep(1)
+        if self.is_playing:
+            if exc_type is None:
+                logger.warning("Exited while process is still running.")
+                logger.info("Hint: Use Bag.wait() or Bag.play(wait=True) "
+                            "to wait until completion.")
+            else:
+                self.stop()
+
+        if exc_type == KeyboardInterrupt:
+            logger.info("User exit.")
+            return True
+        elif exc_type is not None:
+            logger.critical("An error occurred. Exiting.")
+        else:
+            logger.info("Goodbye!")
+
+    def __repr__(self):
+        return "<Bag({})>".format(self.filenames)
