ef389c988fc69bd5eba6e026a4500a90a28f0ae0
Jean Nassar <jn.masasin+git@gmail.com>
2017-09-05 08:41:28 -0600

    Fixes #2. Import to module.

--- pyrosbag/__init__.py
+++ pyrosbag/__init__.py
@@ -4,6 +4,14 @@ Note that, in order to access the data within the bag file, the
 ``rosbag_python`` package is extremely convenient. It is available on PyPI.
 
 """
+from .pyrosbag import (
+    BagError,
+    MissingBagError,
+    BagNotRunningError,
+    Bag,
+    BagPlayer,
+)
+
 __author__ = """Jean Nassar"""
 __email__ = 'jeannassar5@gmail.com'
 __version__ = '0.1.2'
